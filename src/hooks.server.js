6d80f7d6bb155349caba99ffbbe0655c6a927fcb
therealozp <anhkhang.le0910@gmail.com>
2024-11-30 17:43:51 -0500

    route protection

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,1 +1,19 @@
-export { handle } from './auth.js';
+import { redirect } from '@sveltejs/kit';
+import { handle as authenticationHandle } from './auth';
+import { sequence } from '@sveltejs/kit/hooks';
+
+async function authorizationHandle({ event, resolve }) {
+	// Protect any routes under /authenticated
+	if (event.url.pathname.startsWith('/dashboard')) {
+		const session = await event.locals.auth();
+		if (!session) {
+			// Redirect to the signin page
+			throw redirect(303, '/login');
+		}
+	}
+
+	// If the request is still here, just proceed as normally
+	return resolve(event);
+}
+
+export const handle = sequence(authenticationHandle, authorizationHandle);
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import LoginForm from '$lib/components/custom/authentication/LoginForm.svelte';
 	import { page } from '$app/stores';
+	import { redirect } from '@sveltejs/kit';
 
-	console.log($page.data.session);
+	if ($page.data.session) {
+		redirect(302, '/dashboard');
+	}
 </script>
 
 <div class="flex h-screen min-h-screen items-center justify-center">
